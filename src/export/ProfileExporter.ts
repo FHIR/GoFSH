c3093dc0abb1557094dbda9b58ed1288d9176a7a
Chris Moesel <cmoesel@mitre.org>
2020-07-15 23:25:04 -0400

    Add ExtensionExporter
    - Add an ExtensionExporter.
    - Minor refactoring to allow shared code between ExtensionExporter and ProfileExporter

--- src/export/ProfileExporter.ts
+++ src/export/ProfileExporter.ts
@@ -1,31 +1,10 @@
-import { Profile } from 'fsh-sushi/dist/fshtypes';
 import { EOL } from 'os';
+import { Profile } from 'fsh-sushi/dist/fshtypes';
+import { AbstractSDExporter } from './AbstractSDExporter';
 
-export class ProfileExporter {
+export class ProfileExporter extends AbstractSDExporter {
   export(input: Profile): string {
-    const resultLines = [`Profile: ${input.name}`];
-    if (input.parent) {
-      resultLines.push(`Parent: ${input.parent}`);
-    }
-    if (input.id) {
-      resultLines.push(`Id: ${input.id}`);
-    }
-    if (input.title) {
-      resultLines.push(`Title: "${fshifyString(input.title)}"`);
-    }
-    if (input.description) {
-      resultLines.push(`Description: "${fshifyString(input.description)}"`);
-    }
+    const resultLines = this.exportKeywords(input);
     return resultLines.join(EOL);
   }
-}
-
-// Adds expected backslash-escapes to a string to make it a FSH string
-function fshifyString(input: string): string {
-  return input
-    .replace(/\\/g, '\\\\')
-    .replace(/"/g, '\\"')
-    .replace(/\n/g, '\\n')
-    .replace(/\r/g, '\\r')
-    .replace(/\t/g, '\\t');
 }
--- src/export/index.ts
+++ src/export/index.ts
@@ -1,2 +1,3 @@
 // export classes are responsible for converting FSH objects to FSH text
 export * from './ProfileExporter';
+export * from './ExtensionExporter';
