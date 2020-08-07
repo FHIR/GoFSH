89d65d3658faa0e8016a9f35817dca2cc0fbff39
Julia Afeltra <jafeltra@mitre.org>
2020-08-07 09:51:10 -0400

    Write caret rule

--- src/exportable/ExportableCaretValueRule.ts
+++ src/exportable/ExportableCaretValueRule.ts
@@ -1,4 +1,5 @@
-import { fshrules } from 'fsh-sushi';
+import { isUri } from 'valid-url';
+import { fhirtypes, fshrules, fshtypes } from 'fsh-sushi';
 import { ExportableRule } from '.';
 
 export class ExportableCaretValueRule extends fshrules.CaretValueRule implements ExportableRule {
@@ -7,6 +8,26 @@ export class ExportableCaretValueRule extends fshrules.CaretValueRule implements
   }
 
   toFSH(): string {
-    return '// Unimplemented: caret value rule';
+    let fixedValue;
+    if (
+      this.value instanceof fshtypes.FshCanonical ||
+      this.value instanceof fshtypes.FshCode ||
+      this.value instanceof fshtypes.FshQuantity ||
+      this.value instanceof fshtypes.FshRatio ||
+      this.value instanceof fshtypes.FshReference
+    ) {
+      fixedValue = this.value.toString();
+    } else if (this.value instanceof fhirtypes.InstanceDefinition) {
+      fixedValue = this.value._instanceMeta.name;
+    } else if (typeof this.value === 'boolean' || typeof this.value === 'number') {
+      fixedValue = this.value;
+    } else if (typeof this.value === 'string') {
+      if (isUri(this.value)) {
+        fixedValue = this.value;
+      } else {
+        fixedValue = `"${this.value}"`;
+      }
+    }
+    return `* ${this.path !== '' ? this.path + ' ' : ''}^${this.caretPath} = ${fixedValue}`;
   }
 }
