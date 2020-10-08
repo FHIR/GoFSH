2f31893ed498d04a3a3f65dfed8a775ff7faa74b
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2020-10-08 16:39:38 -0400

    Remove default slicing rules (#9)
    * Remove default slicing rules
    
    * Update based on review
    
    - Use regex for determining path ending
    - Use default rules for comparison
    - Only check if multiple slicing.discriminator values at current path

--- src/processor/Package.ts
+++ src/processor/Package.ts
@@ -69,6 +69,7 @@ export class Package {
     this.removeDefaultExtensionContextRules();
     this.removeImpliedZeroToZeroCardRules();
     this.suppressUrlAssignmentOnExtensions();
+    this.removeDefaultSlicingRules();
   }
 
   private resolveProfileParents(processor: FHIRProcessor): void {
@@ -271,5 +272,62 @@ export class Package {
         pullAt(extension.rules, rulesToRemove);
       });
     }
+  }
+
+  private removeDefaultSlicingRules(): void {
+    [...this.profiles, ...this.extensions].forEach(sd => {
+      const rulesToMaybeRemove: number[] = [];
+      sd.rules.forEach((rule, i, allRules) => {
+        if (rule instanceof ExportableCaretValueRule && /(modifierE|e)xtension$/.test(rule.path)) {
+          // * path ^slicing.discriminator[0].type = #value
+          const DEFAULT_SLICING_DISCRIMINATOR_TYPE = new ExportableCaretValueRule(rule.path);
+          DEFAULT_SLICING_DISCRIMINATOR_TYPE.caretPath = 'slicing.discriminator[0].type';
+          DEFAULT_SLICING_DISCRIMINATOR_TYPE.value = new FshCode('value');
+          // * path ^slicing.discriminator[0].value = "url"
+          const DEFAULT_SLICING_DISCRIMINATOR_PATH = new ExportableCaretValueRule(rule.path);
+          DEFAULT_SLICING_DISCRIMINATOR_PATH.caretPath = 'slicing.discriminator[0].path';
+          DEFAULT_SLICING_DISCRIMINATOR_PATH.value = 'url';
+          // * path ^slicing.ordered = false
+          const DEFAULT_SLICING_ORDERED = new ExportableCaretValueRule(rule.path);
+          DEFAULT_SLICING_ORDERED.caretPath = 'slicing.ordered';
+          DEFAULT_SLICING_ORDERED.value = false;
+          // * path ^slicing.rules = #open
+          const DEFAULT_SLICING_RULES = new ExportableCaretValueRule(rule.path);
+          DEFAULT_SLICING_RULES.caretPath = 'slicing.rules';
+          DEFAULT_SLICING_RULES.value = new FshCode('open');
+          const hasContainsRule = allRules.some(
+            otherRule => otherRule instanceof ExportableContainsRule && otherRule.path === rule.path
+          );
+          const hasOneSlicingDiscriminatorRule = !allRules.some(
+            otherRule =>
+              otherRule.path === rule.path &&
+              otherRule instanceof ExportableCaretValueRule &&
+              otherRule.caretPath !== 'slicing.discriminator[0].type' &&
+              otherRule.caretPath !== 'slicing.discriminator[0].path' &&
+              otherRule.caretPath.startsWith('slicing.discriminator[')
+          );
+          if (
+            // One of the four default rules
+            (isEqual(rule, DEFAULT_SLICING_DISCRIMINATOR_TYPE) ||
+              isEqual(rule, DEFAULT_SLICING_DISCRIMINATOR_PATH) ||
+              isEqual(rule, DEFAULT_SLICING_ORDERED) ||
+              isEqual(rule, DEFAULT_SLICING_RULES)) &&
+            // Some contains rule at the same path
+            hasContainsRule &&
+            // No other slicing.discriminator rules at the same path
+            hasOneSlicingDiscriminatorRule
+          ) {
+            rulesToMaybeRemove.push(i);
+          }
+        }
+      });
+      // If four rules to maybe remove have the same path, then that's a full set of defaults, and they are removed
+      const rulesToRemove = flatten(
+        values(groupBy(rulesToMaybeRemove, i => sd.rules[i].path)).filter(
+          ruleGroup => ruleGroup.length === 4
+        )
+      );
+      pullAt(sd.rules, rulesToRemove);
+    });
   }
 }
