3ab934262569263b2bd565a4f2b6f40c42fbb7e7
Chris Moesel <cmoesel@users.noreply.github.com>
2020-10-08 14:37:20 -0400

    Don't export card rules for sliced cards that SUSHI calculates (#12)
    * Suppress cardinality mins when they match sum of slices' mins
    
    For sliced elements, SUSHI will automatically set the min to the sum of all the slices' mins.  Since this happens, we don't need to export a rule if it matchs the same number that SUSHI would produce.
    
    * Only remove calculated cards on sliced elements if there are local card constraints on slices.
    
    * Fix test descriptions

--- src/rule-extractor/CardRuleExtractor.ts
+++ src/rule-extractor/CardRuleExtractor.ts
@@ -1,9 +1,15 @@
-import { ProcessableElementDefinition } from '../processor';
+import { fhirdefs } from 'fsh-sushi';
+import { ProcessableStructureDefinition, ProcessableElementDefinition } from '../processor';
 import { ExportableCardRule } from '../exportable';
 import { getPath } from '../utils';
 
 export class CardRuleExtractor {
-  static process(input: ProcessableElementDefinition): ExportableCardRule | null {
+  static process(
+    input: ProcessableElementDefinition,
+    structDef: ProcessableStructureDefinition,
+    fhir: fhirdefs.FHIRDefinitions,
+    removeInferredSlicedMin = true
+  ): ExportableCardRule | null {
     if (input.min || input.max) {
       const cardRule = new ExportableCardRule(getPath(input));
       if (input.min != null) {
@@ -14,9 +20,103 @@ export class CardRuleExtractor {
         cardRule.max = input.max;
         input.processedPaths.push('max');
       }
+      // SUSHI will automatically create a card rule if all of the following is true:
+      // - this is a sliced element
+      // - there is a cardinality rule on at least one of the slices
+      // - the min on this element matches the sum of the mins on all slices
+      // If we reach this condition, don't output a card min since SUSHI will do it.
+      // Technically this is an optimization, but the optimizations don't have access to
+      // the full SD, and we need the full SD to get the sum of all slices (including
+      // inherited slices)
+      if (
+        removeInferredSlicedMin &&
+        hasSlicesWithConstrainedCards(input, structDef) &&
+        isSliced(input, structDef, fhir)
+      ) {
+        const sumOfMins = getSumOfSliceMins(input, structDef, fhir);
+        if (sumOfMins === input.min) {
+          if (cardRule.max == null) {
+            // No min or max, so return null card rule
+            return null;
+          }
+          // has a max, so return cardRule without the min
+          cardRule.min = undefined;
+        }
+      }
       return cardRule;
     } else {
       return null;
     }
   }
+}
+
+function hasSlicesWithConstrainedCards(
+  input: ProcessableElementDefinition,
+  structDef: ProcessableStructureDefinition
+): boolean {
+  return structDef?.differential?.element?.some(
+    el =>
+      el.path == input.path &&
+      el.id != input.id &&
+      el.sliceName != null &&
+      (el.min != null || el.max != null)
+  );
+}
+
+function isSliced(
+  input: ProcessableElementDefinition,
+  structDef: ProcessableStructureDefinition,
+  fhir: fhirdefs.FHIRDefinitions
+): boolean {
+  for (
+    let currentStructDef = structDef;
+    currentStructDef != null;
+    currentStructDef = fhir.fishForFHIR(currentStructDef.baseDefinition)
+  ) {
+    // If the element is in the snapshot, then presence or lack of slicing is conclusive
+    const snapshotEl = currentStructDef.snapshot?.element?.find(el => el.id === input.id);
+    if (snapshotEl) {
+      return snapshotEl.slicing != null;
+    } else {
+      // The differential is only conclusive in the positive case, otherwise it could still be inherited from a parent
+      const differentialEl = currentStructDef.differential?.element?.find(el => el.id === input.id);
+      if (differentialEl?.slicing != null) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function getSumOfSliceMins(
+  input: ProcessableElementDefinition,
+  structDef: ProcessableStructureDefinition,
+  fhir: fhirdefs.FHIRDefinitions
+): number {
+  let sum = 0;
+  const countedSlices = new Set<string>();
+  for (
+    let currentStructDef = structDef;
+    currentStructDef != null;
+    currentStructDef = fhir.fishForFHIR(currentStructDef.baseDefinition)
+  ) {
+    const uncountedSlicesWithMins = (
+      currentStructDef.snapshot ?? currentStructDef.differential
+    )?.element?.filter(
+      el =>
+        el.path === input.path &&
+        el.sliceName != null &&
+        el.min != null &&
+        !countedSlices.has(el.sliceName)
+    );
+    uncountedSlicesWithMins.forEach(slice => {
+      sum += slice.min;
+      countedSlices.add(slice.sliceName);
+    });
+    if (currentStructDef.snapshot != null) {
+      break;
+    }
+  }
+
+  return sum;
 }
