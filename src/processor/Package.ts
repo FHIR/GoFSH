bb4ad77853edc95c8117695cd43b60eea7b173ca
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2020-10-09 10:59:35 -0400

    Suppress ^date rules (#13)
    * Remove ^date rules if dates appear to be set by the IG Publisher
    
    - Dates that appear to be set by the publisher if
        - all dates are the same
        - all dates include a time with seconds
        - all dates are in GMT
    - Add tests
    
    * Remove date rules on value sets and code systems
    
    * Update based on review
    
    - Use for loop and return as soon as any date does not match the others
    - Return earlier to avoid unnecessary checks if no date was ever set
    - Add link to FHIR dateTime regex
    - Simplify check for GMT timezone
    - Filter out rules instead of pull
    - Fix test descriptions
    
    * Use one forEach for all resources and assert rule type. Simplify GMT check
    
    * Fix prettier

--- src/processor/Package.ts
+++ src/processor/Package.ts
@@ -11,7 +11,8 @@ import {
   ExportableContainsRule,
   ExportableOnlyRule,
   ExportableCaretValueRule,
-  ExportableFixedValueRule
+  ExportableFixedValueRule,
+  ExportableRule
 } from '../exportable';
 import { FHIRProcessor } from './FHIRProcessor';
 import { logger } from '../utils';
@@ -70,6 +71,7 @@ export class Package {
     this.removeImpliedZeroToZeroCardRules();
     this.suppressUrlAssignmentOnExtensions();
     this.removeDefaultSlicingRules();
+    this.removeDateRules();
   }
 
   private resolveProfileParents(processor: FHIRProcessor): void {
@@ -329,5 +331,56 @@ export class Package {
       );
       pullAt(sd.rules, rulesToRemove);
     });
+  }
+
+  private removeDateRules(): void {
+    let allDatesMatch = true;
+    let date: string;
+    for (const resource of [
+      ...this.profiles,
+      ...this.extensions,
+      ...this.valueSets,
+      ...this.codeSystems
+    ]) {
+      for (const rule of resource.rules) {
+        if (
+          rule instanceof ExportableCaretValueRule &&
+          rule.caretPath === 'date' &&
+          rule.path === ''
+        ) {
+          const dateValue = rule.value as string; // If the rule assigns a date, the value will be a string.
+          if (!date) date = dateValue; // Set the date to match
+          if (date !== dateValue) {
+            allDatesMatch = false;
+            return; // If any date hasn't matched the others, we don't want to remove any rules.
+          }
+        }
+      }
+    }
+
+    // If there are no date CaretValueRules found, just return.
+    if (date == null) {
+      return;
+    }
+
+    // If all dates are the same, are defined to the second, and are in GMT,
+    // we can assume they were set by the IG Publisher and can be safely removed.
+    // Make sure the value matches one allowed by the dateTime type in FHIR.
+    // See: http://hl7.org/fhir/R4/datatypes.html#dateTime
+    const dateTimeRegex = /^([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1])(T([01][0-9]|2[0-3]):[0-5][0-9]:([0-5][0-9]|60)(\.[0-9]+)?(Z|(\+|-)((0[0-9]|1[0-3]):[0-5][0-9]|14:00)))?)?)?$/;
+    const dateTimeMatch = date.match(dateTimeRegex);
+    const usesGMT = dateTimeMatch && date.endsWith('+00:00'); // If it is a valid FHIR dateTime, check that it uses GMT time zone
+    if (allDatesMatch && usesGMT) {
+      const DEFAULT_DATE = new ExportableCaretValueRule('');
+      DEFAULT_DATE.caretPath = 'date';
+      DEFAULT_DATE.value = date;
+      [...this.profiles, ...this.extensions, ...this.valueSets, ...this.codeSystems].forEach(
+        resource => {
+          (resource.rules as ExportableRule[]) = (resource.rules as ExportableRule[]).filter(
+            rule => !isEqual(rule, DEFAULT_DATE)
+          );
+        }
+      );
+    }
   }
 }
