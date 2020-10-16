44d6fbf65c30daddcc8a28276b3a77d8d4824983
Mint Thompson <mathompson@mitre.org>
2020-10-16 12:30:30 -0400

    Infer additional attributes when extracting configuration
    The fhirVersion, version, and status attributes can be directly inferred
    from rules on resources. The id and name attributes can be inferred
    based on the canonical.

--- src/extractor/ConfigurationExtractor.ts
+++ src/extractor/ConfigurationExtractor.ts
@@ -1,15 +1,39 @@
+import { countBy, toPairs, maxBy, capitalize } from 'lodash';
+import { ImplementationGuideStatus } from 'fsh-sushi/dist/fhirtypes/ImplementationGuide';
 import { Package } from '../processor';
-import { ExportableConfiguration } from '../exportable';
-import { CaretValueRule } from 'fsh-sushi/dist/fshtypes/rules';
-import { countBy, toPairs, maxBy } from 'lodash';
+import { ExportableConfiguration, ExportableCaretValueRule, ExportableRule } from '../exportable';
+import { fshtypes } from 'fsh-sushi';
 
 export class ConfigurationExtractor {
+  static canonicalRegex = /^https?:\/\/(\w+)\.[\w.]+\/(.*)$/;
+
   static process(resources: Package): ExportableConfiguration {
+    // canonical and fhirVersion are required elements in configuration
     const canonical = ConfigurationExtractor.inferCanonical(resources);
+    const fhirVersion = ConfigurationExtractor.inferString(resources, 'fhirVersion');
     const config = new ExportableConfiguration({
       canonical: canonical,
-      fhirVersion: ['4.0.1']
+      fhirVersion: [fhirVersion]
     });
+    // infer id and name using canonical
+    const canonicalMatch = canonical.match(ConfigurationExtractor.canonicalRegex);
+    if (canonicalMatch) {
+      const idParts = [canonicalMatch[1], ...canonicalMatch[2].split('/')];
+      config.config.id = idParts.join('.');
+      config.config.name = idParts.map(capitalize).join('');
+    }
+    // infer status and version from most common values
+    const status = ConfigurationExtractor.inferString(
+      resources,
+      'status'
+    ) as ImplementationGuideStatus;
+    const version = ConfigurationExtractor.inferString(resources, 'version');
+    if (status) {
+      config.config.status = status;
+    }
+    if (version) {
+      config.config.version = version;
+    }
     return config;
   }
 
@@ -18,8 +42,10 @@ export class ConfigurationExtractor {
     const potentialCanonicals: string[] = [];
     [...resources.profiles, ...resources.extensions].forEach(resource => {
       const urlRule = resource.rules.find(rule => {
-        return rule instanceof CaretValueRule && rule.path === '' && rule.caretPath === 'url';
-      }) as CaretValueRule;
+        return (
+          rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
+        );
+      }) as ExportableCaretValueRule;
       if (urlRule) {
         potentialCanonicals.push(
           urlRule.value.toString().replace(/\/StructureDefinition\/[^/]+$/, '')
@@ -28,23 +54,58 @@ export class ConfigurationExtractor {
     });
     resources.valueSets.forEach(valueSet => {
       const urlRule = valueSet.rules.find(rule => {
-        return rule instanceof CaretValueRule && rule.path === '' && rule.caretPath === 'url';
-      }) as CaretValueRule;
+        return (
+          rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
+        );
+      }) as ExportableCaretValueRule;
       if (urlRule) {
         potentialCanonicals.push(urlRule.value.toString().replace(/\/ValueSet\/[^/]+$/, ''));
       }
     });
     resources.codeSystems.forEach(codeSystem => {
       const urlRule = codeSystem.rules.find(rule => {
-        return rule instanceof CaretValueRule && rule.path === '' && rule.caretPath === 'url';
-      }) as CaretValueRule;
+        return (
+          rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
+        );
+      }) as ExportableCaretValueRule;
       if (urlRule) {
         potentialCanonicals.push(urlRule.value.toString().replace(/\/CodeSystem\/[^/]+$/, ''));
       }
     });
 
-    if (potentialCanonicals.length > 0) {
-      return maxBy(toPairs(countBy(potentialCanonicals)), pair => pair[1])[0];
+    return ConfigurationExtractor.getStringMode(potentialCanonicals);
+  }
+
+  static inferString(resources: Package, caretPath: string): string {
+    const potentialStrings: string[] = [];
+    [
+      ...resources.profiles,
+      ...resources.extensions,
+      ...resources.valueSets,
+      ...resources.codeSystems
+    ].forEach(resource => {
+      const caretRule = (resource.rules as ExportableRule[]).find(rule => {
+        return (
+          rule instanceof ExportableCaretValueRule &&
+          rule.path === '' &&
+          rule.caretPath === caretPath
+        );
+      }) as ExportableCaretValueRule;
+      if (caretRule) {
+        // if we find a FshCode, we only want the "code" part
+        if (caretRule.value instanceof fshtypes.FshCode) {
+          potentialStrings.push(caretRule.value.code);
+        } else {
+          potentialStrings.push(caretRule.value.toString());
+        }
+      }
+    });
+    return ConfigurationExtractor.getStringMode(potentialStrings);
+  }
+
+  private static getStringMode(potentials: string[]): string {
+    if (potentials.length > 0) {
+      return maxBy(toPairs(countBy(potentials)), pair => pair[1])[0];
     } else {
       return '';
     }
--- test/extractor/ConfigurationExtractor.test.ts
+++ test/extractor/ConfigurationExtractor.test.ts
@@ -1,3 +1,5 @@
+import { cloneDeep } from 'lodash';
+import { fshtypes } from 'fsh-sushi';
 import { ConfigurationExtractor } from '../../src/extractor';
 import { Package } from '../../src/processor';
 import {
@@ -7,55 +9,107 @@ import {
   ExportableValueSet,
   ExportableCodeSystem
 } from '../../src/exportable';
-import { cloneDeep } from 'lodash';
 
 describe('ConfigurationExtractor', () => {
+  let resources: Package;
+
+  beforeAll(() => {
+    resources = new Package();
+    const typicalProfile = new ExportableProfile('TypicalProfile');
+    const profileUrl = new ExportableCaretValueRule('');
+    profileUrl.caretPath = 'url';
+    profileUrl.value = 'http://example.org/our/ig/StructureDefinition/TypicalProfile';
+    typicalProfile.rules.push(profileUrl);
+
+    const typicalExtension = new ExportableExtension('TypicalExtension');
+    const extensionUrl = cloneDeep(profileUrl);
+    extensionUrl.value = 'http://example.org/our/ig/StructureDefinition/TypicalExtension';
+    const extensionStatus = new ExportableCaretValueRule('');
+    extensionStatus.caretPath = 'status';
+    extensionStatus.value = new fshtypes.FshCode('draft');
+    const extensionVersion = new ExportableCaretValueRule('');
+    extensionVersion.caretPath = 'version';
+    extensionVersion.value = '0.8.6';
+    const extensionFhirVersion = new ExportableCaretValueRule('');
+    extensionFhirVersion.caretPath = 'fhirVersion';
+    extensionFhirVersion.value = '4.0.1';
+    typicalExtension.rules.push(extensionUrl, extensionStatus, extensionVersion);
+
+    const typicalValueSet = new ExportableValueSet('TypicalVS');
+    const valueSetUrl = cloneDeep(profileUrl);
+    valueSetUrl.value = 'http://example.org/our/ig/ValueSet/TypicalVS';
+    typicalValueSet.rules.push(
+      valueSetUrl,
+      cloneDeep(extensionStatus),
+      cloneDeep(extensionVersion),
+      cloneDeep(extensionFhirVersion)
+    );
+
+    const typicalCodeSystem = new ExportableCodeSystem('TypicalCodes');
+    const codeSystemUrl = cloneDeep(profileUrl);
+    codeSystemUrl.value = 'http://example.org/our/ig/CodeSystem/TypicalCodes';
+    const codeSystemStatus = cloneDeep(extensionStatus);
+    codeSystemStatus.value = new fshtypes.FshCode('active');
+    typicalCodeSystem.rules.push(
+      codeSystemUrl,
+      codeSystemStatus,
+      cloneDeep(extensionVersion),
+      cloneDeep(extensionFhirVersion)
+    );
+
+    resources.add(typicalProfile);
+    resources.add(typicalExtension);
+    resources.add(typicalValueSet);
+    resources.add(typicalCodeSystem);
+  });
+
   describe('#process', () => {
-    it('should return a configuration with a canonical and default fhirVersion when there are no resources', () => {
+    it('should return a configuration with a canonical and fhirVersion when there are no resources', () => {
       const emptyResources = new Package();
       const result = ConfigurationExtractor.process(emptyResources);
       expect(result.config.canonical).toBeDefined();
+      expect(result.config.fhirVersion).toBeDefined();
+    });
+
+    it('should return a configuration with elements inferred from the canonical', () => {
+      const result = ConfigurationExtractor.process(resources);
+      expect(result.config.canonical).toBe('http://example.org/our/ig');
+      expect(result.config.id).toBe('example.our.ig');
+      expect(result.config.name).toBe('ExampleOurIg');
+    });
+
+    it('should return a configuration with elements inferred from resources', () => {
+      const result = ConfigurationExtractor.process(resources);
       expect(result.config.fhirVersion).toEqual(['4.0.1']);
+      expect(result.config.version).toBe('0.8.6');
+      expect(result.config.status).toBe('draft');
     });
   });
 
   describe('#inferCanonical', () => {
-    let resources: Package;
-
-    beforeAll(() => {
-      resources = new Package();
-      const typicalProfile = new ExportableProfile('TypicalProfile');
-      const profileUrl = new ExportableCaretValueRule('');
-      profileUrl.caretPath = 'url';
-      profileUrl.value = 'http://example.org/our-ig/StructureDefinition/TypicalProfile';
-      typicalProfile.rules.push(profileUrl);
-      const typicalExtension = new ExportableExtension('TypicalExtension');
-      const extensionUrl = cloneDeep(profileUrl);
-      extensionUrl.value = 'http://example.org/our-ig/StructureDefinition/TypicalExtension';
-      typicalExtension.rules.push(extensionUrl);
-      const typicalValueSet = new ExportableValueSet('TypicalVS');
-      const valueSetUrl = cloneDeep(profileUrl);
-      valueSetUrl.value = 'http://example.org/our-ig/ValueSet/TypicalVS';
-      typicalValueSet.rules.push(valueSetUrl);
-      const typicalCodeSystem = new ExportableCodeSystem('TypicalCodes');
-      const codeSystemUrl = cloneDeep(profileUrl);
-      codeSystemUrl.value = 'http://example.org/our-ig/CodeSystem/TypicalCodes';
-      typicalCodeSystem.rules.push(codeSystemUrl);
-      resources.add(typicalProfile);
-      resources.add(typicalExtension);
-      resources.add(typicalValueSet);
-      resources.add(typicalCodeSystem);
-    });
-
     it('should return an empty string when the input contains no resources', () => {
       const emptyResources = new Package();
-      const canonical = ConfigurationExtractor.inferCanonical(emptyResources);
-      expect(canonical).toBe('');
+      const result = ConfigurationExtractor.inferCanonical(emptyResources);
+      expect(result).toBe('');
+    });
+
+    it('should return an empty string when none of the resources have a defined url', () => {
+      const noUrlResources = cloneDeep(resources);
+      // replace the rules that set a url with some empty rules
+      const differentRule = new ExportableCaretValueRule('');
+      differentRule.caretPath = 'publisher';
+      differentRule.value = 'Somebody';
+      noUrlResources.profiles[0].rules[0] = differentRule;
+      noUrlResources.extensions[0].rules[0] = cloneDeep(differentRule);
+      noUrlResources.valueSets[0].rules[0] = cloneDeep(differentRule);
+      noUrlResources.codeSystems[0].rules[0] = cloneDeep(differentRule);
+      const result = ConfigurationExtractor.inferCanonical(noUrlResources);
+      expect(result).toBe('');
     });
 
     it('should use the common url base to determine the canonical when all resources have the same url base', () => {
       const result = ConfigurationExtractor.inferCanonical(resources);
-      expect(result).toBe('http://example.org/our-ig');
+      expect(result).toBe('http://example.org/our/ig');
     });
 
     it('should use the most frequent url base to determine the canonical when not all resources have the same url base', () => {
@@ -63,12 +117,12 @@ describe('ConfigurationExtractor', () => {
       const differentProfile = new ExportableProfile('DifferentProfile');
       const differentUrl = new ExportableCaretValueRule('');
       differentUrl.caretPath = 'url';
-      differentUrl.value = 'http://example.org/different-ig/StructureDefinition/DifferentProfile';
+      differentUrl.value = 'http://example.org/different/ig/StructureDefinition/DifferentProfile';
       differentProfile.rules.push(differentUrl);
       differentResources.add(differentProfile);
       const result = ConfigurationExtractor.inferCanonical(differentResources);
 
-      expect(result).toBe('http://example.org/our-ig');
+      expect(result).toBe('http://example.org/our/ig');
     });
   });
 });
