28ad6f91c5d5955b9cf8d08648ed93ce5771e398
Mint Thompson <mathompson@mitre.org>
2020-10-21 12:58:00 -0400

    Improve inference of name and id from canonical
    A valid canonical URL can take many forms. The function to use the
    canonical URL to determine the name and id for a configuration now
    correctly handles a greater variety of URLs. Test cases are added to
    demonstrate these possibilities. The code for inferring the name and id
    is moved to a separate function for ease of testing.
    
    In ConfigurationExtractor, clean up imports and add an explanation to
    the relatively dense getStringMode function. Use default values for
    canonical and FHIRVersion when inferred values are unavailable.

--- src/extractor/ConfigurationExtractor.ts
+++ src/extractor/ConfigurationExtractor.ts
@@ -1,36 +1,31 @@
-import { countBy, toPairs, maxBy, capitalize } from 'lodash';
-import { ImplementationGuideStatus } from 'fsh-sushi/dist/fhirtypes/ImplementationGuide';
+import { countBy, toPairs, maxBy, capitalize, filter } from 'lodash';
+import { fshtypes, fhirtypes } from 'fsh-sushi';
 import { Package } from '../processor';
 import { ExportableConfiguration, ExportableCaretValueRule, ExportableRule } from '../exportable';
-import { fshtypes } from 'fsh-sushi';
+
+type ImplementationGuideStatus = fhirtypes.ImplementationGuideStatus;
+const { FshCode } = fshtypes;
 
 export class ConfigurationExtractor {
-  static canonicalRegex = /^https?:\/\/(\w+)\.[\w.]+\/(.*)$/;
-
   static process(resources: Package): ExportableConfiguration {
-    // canonical and fhirVersion are required elements in configuration
-    const canonical = ConfigurationExtractor.inferCanonical(resources);
-    const fhirVersion = ConfigurationExtractor.inferString(resources, 'fhirVersion');
+    // canonical and fhirVersion are required elements in configuration, so they get defaults
+    const canonical = ConfigurationExtractor.inferCanonical(resources) || 'http://sample.org';
+    const fhirVersion = ConfigurationExtractor.inferString(resources, 'fhirVersion') || '4.0.1';
     const config = new ExportableConfiguration({
       canonical: canonical,
       fhirVersion: [fhirVersion]
     });
-    // infer id and name using canonical
-    const canonicalMatch = canonical.match(ConfigurationExtractor.canonicalRegex);
-    if (canonicalMatch) {
-      const idParts = [canonicalMatch[1], ...canonicalMatch[2].split('/')];
-      config.config.id = idParts.join('.');
-      config.config.name = idParts.map(capitalize).join('');
-    }
+    // infer name and id using canonical
+    Object.assign(config.config, ConfigurationExtractor.inferNameAndId(config));
     // infer status and version from most common values
     const status = ConfigurationExtractor.inferString(
       resources,
       'status'
     ) as ImplementationGuideStatus;
-    const version = ConfigurationExtractor.inferString(resources, 'version');
     if (status) {
       config.config.status = status;
     }
+    const version = ConfigurationExtractor.inferString(resources, 'version');
     if (version) {
       config.config.version = version;
     }
@@ -40,40 +35,47 @@ export class ConfigurationExtractor {
   static inferCanonical(resources: Package): string {
     // url may be present as a CaretValueRule, path = '', caret path = 'url'
     const potentialCanonicals: string[] = [];
-    [...resources.profiles, ...resources.extensions].forEach(resource => {
-      const urlRule = resource.rules.find(rule => {
+    [
+      ...resources.profiles,
+      ...resources.extensions,
+      ...resources.valueSets,
+      ...resources.codeSystems
+    ].forEach(resource => {
+      const urlRule = (resource.rules as ExportableRule[]).find(rule => {
         return (
           rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
         );
       }) as ExportableCaretValueRule;
       if (urlRule) {
         potentialCanonicals.push(
-          urlRule.value.toString().replace(/\/StructureDefinition\/[^/]+$/, '')
+          urlRule.value
+            .toString()
+            .replace(/\/(StructureDefinition|ValueSet|CodeSystem)\/[^/]+$/, '')
         );
-      }
-    });
-    resources.valueSets.forEach(valueSet => {
-      const urlRule = valueSet.rules.find(rule => {
-        return (
-          rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
-        );
-      }) as ExportableCaretValueRule;
-      if (urlRule) {
-        potentialCanonicals.push(urlRule.value.toString().replace(/\/ValueSet\/[^/]+$/, ''));
-      }
-    });
-    resources.codeSystems.forEach(codeSystem => {
-      const urlRule = codeSystem.rules.find(rule => {
-        return (
-          rule instanceof ExportableCaretValueRule && rule.path === '' && rule.caretPath === 'url'
-        );
-      }) as ExportableCaretValueRule;
-      if (urlRule) {
-        potentialCanonicals.push(urlRule.value.toString().replace(/\/CodeSystem\/[^/]+$/, ''));
       }
     });
 
     return ConfigurationExtractor.getStringMode(potentialCanonicals);
+  }
+
+  // The regular expression for matching the canonical won't behave perfectly in all cases,
+  // due to the sheer variety of possible valid URLs. But, it tries its best.
+  static inferNameAndId(config: ExportableConfiguration): { name?: string; id?: string } {
+    const canonicalMatch = config.config.canonical.match(/^https?:\/\/([\w.]+)(\/.*)?$/);
+    if (canonicalMatch) {
+      const hostParts = canonicalMatch[1].split('.').slice(0, -1);
+      const pathParts = canonicalMatch[2]?.split('/') ?? [];
+      const idParts = [
+        ...filter(hostParts, part => part.length > 2 && part !== 'www'),
+        ...filter(pathParts, part => part.length > 0)
+      ];
+      return {
+        name: idParts.map(capitalize).join(''),
+        id: idParts.join('.')
+      };
+    } else {
+      return {};
+    }
   }
 
   static inferString(resources: Package, caretPath: string): string {
@@ -93,7 +95,7 @@ export class ConfigurationExtractor {
       }) as ExportableCaretValueRule;
       if (caretRule) {
         // if we find a FshCode, we only want the "code" part
-        if (caretRule.value instanceof fshtypes.FshCode) {
+        if (caretRule.value instanceof FshCode) {
           potentialStrings.push(caretRule.value.code);
         } else {
           potentialStrings.push(caretRule.value.toString());
@@ -105,6 +107,12 @@ export class ConfigurationExtractor {
 
   private static getStringMode(potentials: string[]): string {
     if (potentials.length > 0) {
+      // Example results after each function
+      //   potentials: ['foo', 'bar', 'baz', 'foo', 'bar', 'bar']
+      //   countBy:    { 'foo': 2, 'bar': 3, 'baz': 1 }
+      //   toPairs:    [ ['foo', 2], ['bar': 3], ['baz': 1] ]
+      //   maxBy:      ['bar', 3]
+      //   maxyBy[0]:  'bar'
       return maxBy(toPairs(countBy(potentials)), pair => pair[1])[0];
     } else {
       return '';
--- test/extractor/ConfigurationExtractor.test.ts
+++ test/extractor/ConfigurationExtractor.test.ts
@@ -7,7 +7,8 @@ import {
   ExportableCaretValueRule,
   ExportableExtension,
   ExportableValueSet,
-  ExportableCodeSystem
+  ExportableCodeSystem,
+  ExportableConfiguration
 } from '../../src/exportable';
 
 describe('ConfigurationExtractor', () => {
@@ -64,11 +65,11 @@ describe('ConfigurationExtractor', () => {
   });
 
   describe('#process', () => {
-    it('should return a configuration with a canonical and fhirVersion when there are no resources', () => {
+    it('should return a configuration with default canonical and fhirVersion when there are no resources', () => {
       const emptyResources = new Package();
       const result = ConfigurationExtractor.process(emptyResources);
-      expect(result.config.canonical).toBeDefined();
-      expect(result.config.fhirVersion).toBeDefined();
+      expect(result.config.canonical).toBe('http://sample.org');
+      expect(result.config.fhirVersion).toEqual(['4.0.1']);
     });
 
     it('should return a configuration with elements inferred from the canonical', () => {
@@ -123,6 +124,167 @@ describe('ConfigurationExtractor', () => {
       const result = ConfigurationExtractor.inferCanonical(differentResources);
 
       expect(result).toBe('http://example.org/our/ig');
+    });
+  });
+
+  describe('#inferNameAndId', () => {
+    it('should return an empty object when the canonical does not match the standard format', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'http://bad-canonical',
+        fhirVersion: []
+      });
+      const result = ConfigurationExtractor.inferNameAndId(config);
+      expect(result).toEqual({});
+    });
+
+    it('should return a name and id when the canonical has a two-part host and no path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'https://example.org',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'Example',
+        id: 'example'
+      });
+
+      config.config.canonical = 'http://example.org/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'Example',
+        id: 'example'
+      });
+    });
+
+    it('should return a name and id when the canonical has a two-part host and a non-empty path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'https://example.org/first',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleFirst',
+        id: 'example.first'
+      });
+
+      config.config.canonical = 'https://example.org/first/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleFirst',
+        id: 'example.first'
+      });
+
+      config.config.canonical = 'https://example.org/first/second';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleFirstSecond',
+        id: 'example.first.second'
+      });
+
+      config.config.canonical = 'http://example.org/first/second/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleFirstSecond',
+        id: 'example.first.second'
+      });
+    });
+
+    it('should return a name and id when the canonical has a three-part host and no path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'http://www.example.org',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'Example',
+        id: 'example'
+      });
+
+      config.config.canonical = 'https://www.example.org/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'Example',
+        id: 'example'
+      });
+
+      config.config.canonical = 'https://fhir.example.org';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'FhirExample',
+        id: 'fhir.example'
+      });
+
+      config.config.canonical = 'http://fhir.example.org/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'FhirExample',
+        id: 'fhir.example'
+      });
+    });
+
+    it('should return a name and id when the canonical has a three-part host and a non-empty path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'http://www.example.org/orange/apple',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleOrangeApple',
+        id: 'example.orange.apple'
+      });
+
+      config.config.canonical = 'https://www.example.org/orange/apple/banana';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleOrangeAppleBanana',
+        id: 'example.orange.apple.banana'
+      });
+
+      config.config.canonical = 'https://fhir.example.org/orange/apple/banana';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'FhirExampleOrangeAppleBanana',
+        id: 'fhir.example.orange.apple.banana'
+      });
+
+      config.config.canonical = 'http://fhir.example.org/orange/apple/banana/pear';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'FhirExampleOrangeAppleBananaPear',
+        id: 'fhir.example.orange.apple.banana.pear'
+      });
+    });
+
+    it('should return a name and id when the canonical has a four or more-part host and no path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'http://www.crate.fruit.org',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'CrateFruit',
+        id: 'crate.fruit'
+      });
+
+      config.config.canonical = 'https://www.example.co.uk/';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'Example',
+        id: 'example'
+      });
+    });
+
+    it('should return a name and id when the canonical has a four or more-part host and a non-empty path', () => {
+      const config = new ExportableConfiguration({
+        canonical: 'http://www.crate.fruit.org/orange/apple',
+        fhirVersion: []
+      });
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'CrateFruitOrangeApple',
+        id: 'crate.fruit.orange.apple'
+      });
+
+      config.config.canonical = 'https://www.example.co.uk/orange/apple/banana';
+      expect(ConfigurationExtractor.inferNameAndId(config)).toEqual({
+        name: 'ExampleOrangeAppleBanana',
+        id: 'example.orange.apple.banana'
+      });
+    });
+  });
+
+  describe('#inferString', () => {
+    it('should return the most common string for the specified caret path', () => {
+      const result = ConfigurationExtractor.inferString(resources, 'version');
+      expect(result).toBe('0.8.6');
+    });
+
+    it('should return the most common code for the specified caret path when it refers to a FshCode', () => {
+      const result = ConfigurationExtractor.inferString(resources, 'status');
+      expect(result).toBe('draft');
     });
   });
 });
