2f75e0189dc193855b0bb1ab2b3e12c86da6f14a
Nicholas Freiter <nfreiter@mitre.org>
2020-11-21 11:00:35 -0500

    Construct inline instances from instances

---
+++ src/optimizer/plugins/ConstructInlineInstanceOptimizer.ts
@@ -0,0 +1,68 @@
+import { pullAt, escapeRegExp } from 'lodash';
+import { OptimizerPlugin } from '../OptimizerPlugin';
+import { Package } from '../../processor';
+import { ExportableAssignmentRule, ExportableInstance } from '../../exportable';
+
+export default {
+  name: 'construct_inline_instance',
+  description:
+    'Construct inline instances from groups of rules in a contained resource or a Bundle',
+
+  optimize(pkg: Package): void {
+    const inlineInstances: ExportableInstance[] = [];
+    [...pkg.instances].forEach(instance => {
+      // First get all possible paths for which child elements should be extracted onto an inline instance
+      const possibleBasePaths = instance.rules
+        .filter(
+          rule =>
+            rule instanceof ExportableAssignmentRule &&
+            (/^contained(\[\d+\])?\.resourceType$/.test(rule.path) ||
+              /^entry(\[\d+\])?\.resource\.resourceType$/.test(rule.path))
+        )
+        .map(rule => rule.path.replace('.resourceType', ''));
+
+      let generatedIdCount = 0;
+      // For each base path, extract an inline instance
+      possibleBasePaths.forEach(possibleBasePath => {
+        const rulesToRemove: number[] = [];
+        let id: string;
+        let resourceType: string;
+        let profile: string;
+        const newInstance = new ExportableInstance('');
+
+        // Find all rules on the instance that are children of the base path and should be
+        // added to the inline instance
+        instance.rules.forEach((rule, i) => {
+          if (
+            !(rule instanceof ExportableAssignmentRule && rule.path.startsWith(possibleBasePath))
+          ) {
+            return;
+          }
+
+          rulesToRemove.push(i);
+          // id and resourceType and meta.profile should be used for keywords, all other rules are added
+          if (rule.path === `${possibleBasePath}.id`) {
+            id = rule.value as string;
+          } else if (rule.path === `${possibleBasePath}.resourceType`) {
+            resourceType = rule.value as string;
+          } else if (
+            new RegExp(`${escapeRegExp(possibleBasePath)}\\.meta\\.profile(\\[0\\])?`).test(
+              rule.path
+            )
+          ) {
+            profile = rule.value as string;
+          } else {
+            newInstance.rules.push(rule);
+          }
+        });
+
+        newInstance.id = id ?? `Inline-Instance-for-${instance.id}-${(generatedIdCount += 1)}`;
+        newInstance.instanceOf = profile ?? resourceType;
+        newInstance.usage = 'Inline';
+        inlineInstances.push(newInstance);
+        pullAt(instance.rules, rulesToRemove);
+      });
+    });
+    pkg.instances.push(...inlineInstances);
+  }
+} as OptimizerPlugin;
---
+++ test/optimizer/plugins/ConstructInlineInstanceOptimizer.test.ts
@@ -0,0 +1,247 @@
+import '../../helpers/loggerSpy'; // side-effect: suppresses logs
+import { Package } from '../../../src/processor';
+import { ExportableAssignmentRule, ExportableInstance } from '../../../src/exportable';
+import optimizer from '../../../src/optimizer/plugins/ConstructInlineInstanceOptimizer';
+import { assertExportableInstance } from '../../helpers/asserts';
+
+describe('optimizer', () => {
+  describe('#construct_inline_instance', () => {
+    let containedResourceType1: ExportableAssignmentRule;
+    let containedId1: ExportableAssignmentRule;
+    let containedResourceType2: ExportableAssignmentRule;
+    let containedId2: ExportableAssignmentRule;
+    let bundleResourceType: ExportableAssignmentRule;
+    let bundleId: ExportableAssignmentRule;
+    let instance: ExportableInstance;
+
+    beforeEach(() => {
+      containedResourceType1 = new ExportableAssignmentRule('contained[0].resourceType');
+      containedResourceType1.value = 'Observation';
+      containedId1 = new ExportableAssignmentRule('contained[0].id');
+      containedId1.value = 'Bar';
+
+      containedResourceType2 = new ExportableAssignmentRule('contained[1].resourceType');
+      containedResourceType2.value = 'ValueSet';
+      containedId2 = new ExportableAssignmentRule('contained[1].id');
+      containedId2.value = 'Baz';
+
+      bundleResourceType = new ExportableAssignmentRule('entry[0].resource.resourceType');
+      bundleResourceType.value = 'Organization';
+      bundleId = new ExportableAssignmentRule('entry[0].resource.id');
+      bundleId.value = 'Bam';
+
+      instance = new ExportableInstance('Foo');
+      instance.instanceOf = 'Patient';
+    });
+
+    it('should have appropriate metadata', () => {
+      expect(optimizer.name).toBe('construct_inline_instance');
+      expect(optimizer.description).toBeDefined();
+      expect(optimizer.runBefore).toBeUndefined();
+      expect(optimizer.runAfter).toBeUndefined();
+    });
+
+    it('should create inline instances from contained resources', () => {
+      instance.rules = [containedResourceType1, containedId1, containedResourceType2, containedId2];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(3);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+      assertExportableInstance(
+        myPackage.instances[2],
+        'Baz',
+        'ValueSet',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create inline instances from contained and bundle resources', () => {
+      instance.rules = [containedResourceType1, containedId1, bundleResourceType, bundleId];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(3);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+      assertExportableInstance(
+        myPackage.instances[2],
+        'Bam',
+        'Organization',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create an inline instance from a contained resource with non-numeric paths', () => {
+      containedResourceType1.path = 'contained.resourceType';
+      containedId1.path = 'contained.id';
+      instance.rules = [containedResourceType1, containedId1];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(2);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create inline instances from contained resources with no id', () => {
+      instance.rules = [containedResourceType1, containedResourceType2];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(3);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Inline-Instance-for-Foo-1',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+      assertExportableInstance(
+        myPackage.instances[2],
+        'Inline-Instance-for-Foo-2',
+        'ValueSet',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create an inline instance from a contained resource with additional rules', () => {
+      const containedString = new ExportableAssignmentRule('contained[0].valueString');
+      containedString.value = 'string value';
+      instance.rules = [containedResourceType1, containedId1, containedString];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(2);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        [containedString]
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create an inline instance from a contained resource and ignore other rules', () => {
+      const containedString = new ExportableAssignmentRule('contained[0].valueString');
+      containedString.value = 'string value';
+
+      const nonContainedRule = new ExportableAssignmentRule('name.family');
+      containedString.value = 'Boo';
+
+      instance.rules = [containedResourceType1, containedId1, containedString, nonContainedRule];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(2);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'Observation',
+        'Inline',
+        undefined,
+        undefined,
+        [containedString]
+      );
+
+      expect(instance.rules).toEqual([nonContainedRule]);
+    });
+
+    it('should create a profiled inline instance from a contained resource', () => {
+      const containedProfile = new ExportableAssignmentRule('contained[0].meta.profile[0]');
+      containedProfile.value =
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab';
+      instance.rules = [containedResourceType1, containedId1, containedProfile];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(2);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+
+    it('should create a profiled inline instance from a contained resource with non-numeric path', () => {
+      const containedProfile = new ExportableAssignmentRule('contained[0].meta.profile');
+      containedProfile.value =
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab';
+      instance.rules = [containedResourceType1, containedId1, containedProfile];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      expect(myPackage.instances).toHaveLength(2);
+      assertExportableInstance(
+        myPackage.instances[1],
+        'Bar',
+        'http://hl7.org/fhir/us/core/StructureDefinition/us-core-observation-lab',
+        'Inline',
+        undefined,
+        undefined,
+        []
+      );
+
+      expect(instance.rules).toHaveLength(0);
+    });
+  });
+});
