ad5e3e6648c13be6f2508621d387ca9f35837706
Nicholas Freiter <nfreiter@mitre.org>
2020-12-02 16:23:01 -0500

    Resolve urls in assignment and caret rules

---
+++ test/optimizer/plugins/ResolveValueRuleURLsOptimizer.test.ts
@@ -0,0 +1,71 @@
+import { cloneDeep } from 'lodash';
+import path from 'path';
+import '../../helpers/loggerSpy'; // side-effect: suppresses logs
+import { utils, fshtypes } from 'fsh-sushi';
+import { Package } from '../../../src/processor';
+import {
+  ExportableAssignmentRule,
+  ExportableCaretValueRule,
+  ExportableInstance,
+  ExportableProfile
+} from '../../../src/exportable';
+import { MasterFisher } from '../../../src/utils';
+import { loadTestDefinitions, stockLake } from '../../helpers';
+import CombineCodingAndQuantityValuesOptimizer from '../../../src/optimizer/plugins/CombineCodingAndQuantityValuesOptimizer';
+import optimizer from '../../../src/optimizer/plugins/ResolveValueRuleURLsOptimizer';
+
+describe('optimizer', () => {
+  describe('#resolve_value_rule_urls', () => {
+    it('should have appropriate metadata', () => {
+      expect(optimizer.name).toBe('resolve_value_rule_urls');
+      expect(optimizer.description).toBeDefined();
+      expect(optimizer.runBefore).toBeUndefined();
+      expect(optimizer.runAfter).toEqual([CombineCodingAndQuantityValuesOptimizer.name]);
+    });
+
+    it('should alias a system in an AssignmentRule', () => {
+      const instance = new ExportableInstance('Foo');
+      instance.instanceOf = 'Observation';
+      const codeRule = new ExportableAssignmentRule('code');
+      codeRule.value = new fshtypes.FshCode('10-9', 'http://loinc.org');
+      instance.rules = [codeRule];
+      const myPackage = new Package();
+      myPackage.add(instance);
+      optimizer.optimize(myPackage);
+
+      const expectedRule = cloneDeep(codeRule);
+      (expectedRule.value as fshtypes.FshCode).system = '$loinc';
+      expect(instance.rules[0]).toEqual(expectedRule);
+    });
+
+    it('should alias a system in an CaretValueRule', () => {
+      const profile = new ExportableProfile('Foo');
+      profile.parent = 'Observation';
+      const jurisdictionRule = new ExportableCaretValueRule('');
+      jurisdictionRule.caretPath = 'jurisdiction';
+      jurisdictionRule.value = new fshtypes.FshCode('MA', 'http://jurisdiction.org/bar');
+      profile.rules = [jurisdictionRule];
+      const myPackage = new Package();
+      myPackage.add(profile);
+      optimizer.optimize(myPackage);
+
+      const expectedRule = cloneDeep(jurisdictionRule);
+      (expectedRule.value as fshtypes.FshCode).system = '$bar';
+      expect(profile.rules[0]).toEqual(expectedRule);
+    });
+
+    it('should maintain the original value when it cannot be aliased', () => {
+      const profile = new ExportableProfile('Foo');
+      profile.parent = 'Observation';
+      const jurisdictionRule = new ExportableCaretValueRule('');
+      jurisdictionRule.caretPath = 'jurisdiction';
+      jurisdictionRule.value = new fshtypes.FshCode('MA', 'urn:oid:123');
+      profile.rules = [jurisdictionRule];
+      const myPackage = new Package();
+      myPackage.add(profile);
+      optimizer.optimize(myPackage);
+
+      expect(profile.rules[0]).toEqual(jurisdictionRule);
+    });
+  });
+});
