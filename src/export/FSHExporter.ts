9b9a504ba5b113857ef509a8a37c46e3327c7d7e
Nicholas Freiter <nfreiter@mitre.org>
2020-12-04 15:12:30 -0500

    Add by-category option for exporting files

--- src/export/FSHExporter.ts
+++ src/export/FSHExporter.ts
@@ -5,7 +5,62 @@ import { logger } from '../utils';
 export class FSHExporter {
   constructor(public readonly fshPackage: Package) {}
 
-  export(): string {
+  export(style: string): Map<string, string> {
+    let exports: Map<string, string>;
+    switch (style) {
+      case 'single-file':
+        exports = this.groupAsSingleFile();
+        break;
+      case 'by-category':
+        exports = this.groupByCategory();
+        break;
+      default:
+        if (style != null) {
+          logger.warn(`Unrecognized output style "${style}". Defaulting to "single-file" style.`);
+        }
+        exports = this.groupAsSingleFile();
+    }
+
+    logger.info(
+      `Exported ${this.fshPackage.profiles.length} Profile${
+        this.fshPackage.profiles.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.extensions.length} Extension${
+        this.fshPackage.extensions.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.codeSystems.length} CodeSystem${
+        this.fshPackage.codeSystems.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.valueSets.length} ValueSet${
+        this.fshPackage.valueSets.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.instances.length} Instance${
+        this.fshPackage.instances.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.invariants.length} Invariant${
+        this.fshPackage.invariants.length == 1 ? '' : 's'
+      }.`
+    );
+    logger.info(
+      `Exported ${this.fshPackage.mappings.length} Mapping${
+        this.fshPackage.mappings.length == 1 ? '' : 's'
+      }.`
+    );
+
+    return exports;
+  }
+
+  private groupAsSingleFile(): Map<string, string> {
     const results: string[] = [];
     if (this.fshPackage.aliases.length > 0) {
       results.push(this.fshPackage.aliases.map(a => a.toFSH()).join(EOL));
@@ -13,60 +68,56 @@ export class FSHExporter {
     for (const profile of this.fshPackage.profiles) {
       results.push(profile.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.profiles.length} Profile${
-        this.fshPackage.profiles.length == 1 ? '' : 's'
-      }.`
-    );
     for (const extension of this.fshPackage.extensions) {
       results.push(extension.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.extensions.length} Extension${
-        this.fshPackage.extensions.length == 1 ? '' : 's'
-      }.`
-    );
     for (const codeSystem of this.fshPackage.codeSystems) {
       results.push(codeSystem.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.codeSystems.length} CodeSystem${
-        this.fshPackage.codeSystems.length == 1 ? '' : 's'
-      }.`
-    );
     for (const valueSet of this.fshPackage.valueSets) {
       results.push(valueSet.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.valueSets.length} ValueSet${
-        this.fshPackage.valueSets.length == 1 ? '' : 's'
-      }.`
-    );
     for (const instance of this.fshPackage.instances) {
       results.push(instance.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.instances.length} Instance${
-        this.fshPackage.instances.length == 1 ? '' : 's'
-      }.`
-    );
     for (const invariant of this.fshPackage.invariants) {
       results.push(invariant.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.invariants.length} Invariant${
-        this.fshPackage.invariants.length == 1 ? '' : 's'
-      }.`
-    );
     for (const mapping of this.fshPackage.mappings) {
       results.push(mapping.toFSH());
     }
-    logger.info(
-      `Exported ${this.fshPackage.mappings.length} Mapping${
-        this.fshPackage.mappings.length == 1 ? '' : 's'
-      }.`
-    );
+    return new Map().set('resources.fsh', results.join(`${EOL}${EOL}`));
+  }
 
-    return results.join(`${EOL}${EOL}`);
+  private groupByCategory(): Map<string, string> {
+    const exports: Map<string, string> = new Map();
+    exports.set(
+      'profiles.fsh',
+      this.fshPackage.profiles.map(profile => profile.toFSH()).join(`${EOL}${EOL}`)
+    );
+    exports.set(
+      'extensions.fsh',
+      this.fshPackage.extensions.map(extension => extension.toFSH()).join(`${EOL}${EOL}`)
+    );
+    exports.set(
+      'terminology.fsh',
+      [
+        ...this.fshPackage.valueSets.map(valueSet => valueSet.toFSH()),
+        ...this.fshPackage.codeSystems.map(codeSystem => codeSystem.toFSH())
+      ].join(`${EOL}${EOL}`)
+    );
+    exports.set(
+      'instances.fsh',
+      this.fshPackage.instances.map(instance => instance.toFSH()).join(`${EOL}${EOL}`)
+    );
+    exports.set(
+      'invariants.fsh',
+      this.fshPackage.invariants.map(invariant => invariant.toFSH()).join(`${EOL}${EOL}`)
+    );
+    exports.set(
+      'mappings.fsh',
+      this.fshPackage.mappings.map(mapping => mapping.toFSH()).join(`${EOL}${EOL}`)
+    );
+    return exports;
   }
 }
--- test/export/FSHExporter.test.ts
+++ test/export/FSHExporter.test.ts
@@ -16,85 +16,10 @@ import { loggerSpy } from '../helpers/loggerSpy';
 describe('FSHExporter', () => {
   let exporter: FSHExporter;
   let myPackage: Package;
-  let profileSpy: jest.SpyInstance;
-  let extensionSpy: jest.SpyInstance;
-  let codeSystemSpy: jest.SpyInstance;
-  let valueSetSpy: jest.SpyInstance;
-  let invariantSpy: jest.SpyInstance;
-  let mappingSpy: jest.SpyInstance;
-  let instanceSpy: jest.SpyInstance;
-  let aliasSpy: jest.SpyInstance;
-
-  beforeAll(() => {
-    profileSpy = jest.spyOn(ExportableProfile.prototype, 'toFSH');
-    extensionSpy = jest.spyOn(ExportableExtension.prototype, 'toFSH');
-    codeSystemSpy = jest.spyOn(ExportableCodeSystem.prototype, 'toFSH');
-    valueSetSpy = jest.spyOn(ExportableValueSet.prototype, 'toFSH');
-    invariantSpy = jest.spyOn(ExportableInvariant.prototype, 'toFSH');
-    mappingSpy = jest.spyOn(ExportableMapping.prototype, 'toFSH');
-    instanceSpy = jest.spyOn(ExportableInstance.prototype, 'toFSH');
-    aliasSpy = jest.spyOn(ExportableAlias.prototype, 'toFSH');
-  });
 
   beforeEach(() => {
     myPackage = new Package();
     exporter = new FSHExporter(myPackage);
-    profileSpy.mockReset();
-    extensionSpy.mockReset();
-    codeSystemSpy.mockReset();
-    valueSetSpy.mockReset();
-    invariantSpy.mockReset();
-    mappingSpy.mockReset();
-    instanceSpy.mockReset();
-    aliasSpy.mockReset();
-  });
-
-  it('should try to export a Profile', () => {
-    myPackage.add(new ExportableProfile('SomeProfile'));
-    exporter.export();
-    expect(profileSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export an Extension', () => {
-    myPackage.add(new ExportableExtension('SomeExtension'));
-    exporter.export();
-    expect(extensionSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export a CodeSystem', () => {
-    myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
-    exporter.export();
-    expect(codeSystemSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export a ValueSet', () => {
-    myPackage.add(new ExportableValueSet('SomeValueSet'));
-    exporter.export();
-    expect(valueSetSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export an Invariant', () => {
-    myPackage.add(new ExportableInvariant('inv-1'));
-    exporter.export();
-    expect(invariantSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export a Mapping', () => {
-    myPackage.add(new ExportableMapping('SomeMapping'));
-    exporter.export();
-    expect(mappingSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export an Instance', () => {
-    myPackage.add(new ExportableInstance('SomeInstance'));
-    exporter.export();
-    expect(instanceSpy).toHaveBeenCalledTimes(1);
-  });
-
-  it('should try to export an Alias', () => {
-    myPackage.aliases.push(new ExportableAlias('LNC', 'http://loinc.org'));
-    exporter.export();
-    expect(aliasSpy).toHaveBeenCalledTimes(1);
   });
 
   it('should log info messages with the number of exported entities', () => {
@@ -104,7 +29,7 @@ describe('FSHExporter', () => {
     myPackage.add(new ExportableValueSet('OneOfThreeValueSets'));
     myPackage.add(new ExportableValueSet('TwoOfThreeValueSets'));
     myPackage.add(new ExportableValueSet('ThreeOfThreeValueSets'));
-    exporter.export();
+    exporter.export('single-file');
     expect(loggerSpy.getMessageAtIndex(-7, 'info')).toBe('Exported 2 Profiles.');
     expect(loggerSpy.getMessageAtIndex(-6, 'info')).toBe('Exported 0 Extensions.');
     expect(loggerSpy.getMessageAtIndex(-5, 'info')).toBe('Exported 1 CodeSystem.');
@@ -114,37 +39,205 @@ describe('FSHExporter', () => {
     expect(loggerSpy.getMessageAtIndex(-1, 'info')).toBe('Exported 0 Mappings.');
   });
 
-  it('should separate each exported result with one blank line', () => {
-    profileSpy.mockRestore();
-    extensionSpy.mockRestore();
-    codeSystemSpy.mockRestore();
-    aliasSpy.mockRestore();
+  it('should log warning when style is unrecognized', () => {
+    exporter.export('foo');
+    expect(loggerSpy.getLastMessage('warn')).toMatch(/Unrecognized output style "foo"/);
+  });
 
-    myPackage.aliases.push(new ExportableAlias('LNC', 'http://loinc.org'));
-    myPackage.add(new ExportableProfile('SomeProfile'));
-    myPackage.add(new ExportableExtension('SomeExtension'));
-    myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
+  describe('#groupBySingleFile', () => {
+    let profileSpy: jest.SpyInstance;
+    let extensionSpy: jest.SpyInstance;
+    let codeSystemSpy: jest.SpyInstance;
+    let valueSetSpy: jest.SpyInstance;
+    let invariantSpy: jest.SpyInstance;
+    let mappingSpy: jest.SpyInstance;
+    let instanceSpy: jest.SpyInstance;
+    let aliasSpy: jest.SpyInstance;
 
-    const result = exporter.export();
-    expect(result).toBe(
-      [
-        'Alias: LNC = http://loinc.org',
-        EOL,
-        EOL,
-        'Profile: SomeProfile',
-        EOL,
-        'Id: SomeProfile',
-        EOL,
-        EOL,
-        'Extension: SomeExtension',
-        EOL,
-        'Id: SomeExtension',
-        EOL,
-        EOL,
-        'CodeSystem: SomeCodeSystem',
-        EOL,
-        'Id: SomeCodeSystem'
-      ].join('')
-    );
+    beforeAll(() => {
+      profileSpy = jest.spyOn(ExportableProfile.prototype, 'toFSH');
+      extensionSpy = jest.spyOn(ExportableExtension.prototype, 'toFSH');
+      codeSystemSpy = jest.spyOn(ExportableCodeSystem.prototype, 'toFSH');
+      valueSetSpy = jest.spyOn(ExportableValueSet.prototype, 'toFSH');
+      invariantSpy = jest.spyOn(ExportableInvariant.prototype, 'toFSH');
+      mappingSpy = jest.spyOn(ExportableMapping.prototype, 'toFSH');
+      instanceSpy = jest.spyOn(ExportableInstance.prototype, 'toFSH');
+      aliasSpy = jest.spyOn(ExportableAlias.prototype, 'toFSH');
+    });
+
+    afterAll(() => {
+      profileSpy.mockRestore();
+      extensionSpy.mockRestore();
+      codeSystemSpy.mockRestore();
+      valueSetSpy.mockRestore();
+      invariantSpy.mockRestore();
+      mappingSpy.mockRestore();
+      instanceSpy.mockRestore();
+      aliasSpy.mockRestore();
+    });
+
+    beforeEach(() => {
+      profileSpy.mockReset();
+      extensionSpy.mockReset();
+      codeSystemSpy.mockReset();
+      valueSetSpy.mockReset();
+      invariantSpy.mockReset();
+      mappingSpy.mockReset();
+      instanceSpy.mockReset();
+      aliasSpy.mockReset();
+    });
+    it('should try to export a Profile', () => {
+      myPackage.add(new ExportableProfile('SomeProfile'));
+      exporter.export('single-file');
+      expect(profileSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export an Extension', () => {
+      myPackage.add(new ExportableExtension('SomeExtension'));
+      exporter.export('single-file');
+      expect(extensionSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export a CodeSystem', () => {
+      myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
+      exporter.export('single-file');
+      expect(codeSystemSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export a ValueSet', () => {
+      myPackage.add(new ExportableValueSet('SomeValueSet'));
+      exporter.export('single-file');
+      expect(valueSetSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export an Invariant', () => {
+      myPackage.add(new ExportableInvariant('inv-1'));
+      exporter.export('single-file');
+      expect(invariantSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export a Mapping', () => {
+      myPackage.add(new ExportableMapping('SomeMapping'));
+      exporter.export('single-file');
+      expect(mappingSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export an Instance', () => {
+      myPackage.add(new ExportableInstance('SomeInstance'));
+      exporter.export('single-file');
+      expect(instanceSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should try to export an Alias', () => {
+      myPackage.aliases.push(new ExportableAlias('LNC', 'http://loinc.org'));
+      exporter.export('single-file');
+      expect(aliasSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should export to a single resources.fsh file when style is "single-file"', () => {
+      profileSpy.mockRestore();
+      extensionSpy.mockRestore();
+      codeSystemSpy.mockRestore();
+
+      myPackage.add(new ExportableProfile('SomeProfile'));
+      myPackage.add(new ExportableExtension('SomeExtension'));
+      myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
+
+      const result = exporter.export('single-file');
+      expect(result).toEqual(
+        new Map().set(
+          'resources.fsh',
+          [
+            'Profile: SomeProfile',
+            EOL,
+            'Id: SomeProfile',
+            EOL,
+            EOL,
+            'Extension: SomeExtension',
+            EOL,
+            'Id: SomeExtension',
+            EOL,
+            EOL,
+            'CodeSystem: SomeCodeSystem',
+            EOL,
+            'Id: SomeCodeSystem'
+          ].join('')
+        )
+      );
+    });
+
+    it('should export to a single resources.fsh file when style is undefined', () => {
+      profileSpy.mockRestore();
+      extensionSpy.mockRestore();
+      codeSystemSpy.mockRestore();
+
+      myPackage.add(new ExportableProfile('SomeProfile'));
+      myPackage.add(new ExportableExtension('SomeExtension'));
+      myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
+
+      const result = exporter.export(undefined);
+      expect(result).toEqual(
+        new Map().set(
+          'resources.fsh',
+          [
+            'Profile: SomeProfile',
+            EOL,
+            'Id: SomeProfile',
+            EOL,
+            EOL,
+            'Extension: SomeExtension',
+            EOL,
+            'Id: SomeExtension',
+            EOL,
+            EOL,
+            'CodeSystem: SomeCodeSystem',
+            EOL,
+            'Id: SomeCodeSystem'
+          ].join('')
+        )
+      );
+    });
+  });
+
+  describe('#groupByCategory', () => {
+    it('should export to a single resources.fsh file when style is "by-category"', () => {
+      myPackage.add(new ExportableProfile('SomeProfile'));
+      myPackage.add(new ExportableExtension('SomeExtension'));
+      myPackage.add(new ExportableValueSet('SomeValueSet'));
+      myPackage.add(new ExportableCodeSystem('SomeCodeSystem'));
+      const instance = new ExportableInstance('SomeInstance');
+      instance.instanceOf = 'SomeProfile';
+      myPackage.add(instance);
+      myPackage.add(new ExportableInvariant('SomeInvariant'));
+      myPackage.add(new ExportableMapping('SomeMapping'));
+
+      const result = exporter.export('by-category');
+      expect(result).toEqual(
+        new Map()
+          .set('profiles.fsh', ['Profile: SomeProfile', EOL, 'Id: SomeProfile'].join(''))
+          .set('extensions.fsh', ['Extension: SomeExtension', EOL, 'Id: SomeExtension'].join(''))
+          .set(
+            'terminology.fsh',
+            [
+              'ValueSet: SomeValueSet',
+              EOL,
+              'Id: SomeValueSet',
+              EOL,
+              EOL,
+              'CodeSystem: SomeCodeSystem',
+              EOL,
+              'Id: SomeCodeSystem'
+            ].join('')
+          )
+          .set(
+            'instances.fsh',
+            ['Instance: SomeInstance', EOL, 'InstanceOf: SomeProfile', EOL, 'Usage: #example'].join(
+              ''
+            )
+          )
+          .set('invariants.fsh', ['Invariant: SomeInvariant'].join(''))
+          .set('mappings.fsh', ['Mapping: SomeMapping', EOL, 'Id: SomeMapping'].join(''))
+      );
+    });
   });
 });
