975b90a8ef33a1492ab66273569ea73db8d7c0fe
Julia Afeltra <jafeltra@mitre.org>
2021-02-12 14:19:54 -0500

    Fshify strings in ConceptRules

--- src/exportable/ExportableConceptRule.ts
+++ src/exportable/ExportableConceptRule.ts
@@ -1,4 +1,5 @@
 import { fshrules } from 'fsh-sushi';
+import { fshifyString } from './common';
 import { ExportableRule } from '.';
 
 export class ExportableConceptRule extends fshrules.ConceptRule implements ExportableRule {
@@ -9,13 +10,13 @@ export class ExportableConceptRule extends fshrules.ConceptRule implements Expor
   toFSH(): string {
     let line = `* #${this.code}`;
     if (this.display) {
-      line += ` "${this.display}"`;
+      line += ` "${fshifyString(this.display)}"`;
     }
     if (this.definition) {
       // If there is no display, a definition must be specified with triple quotes
       // so that it is correctly differentiated from a display by sushi
       const quotes = this.display ? '"' : '"""';
-      line += ` ${quotes}${this.definition}${quotes}`;
+      line += ` ${quotes}${fshifyString(this.definition)}${quotes}`;
     }
     return line;
   }
