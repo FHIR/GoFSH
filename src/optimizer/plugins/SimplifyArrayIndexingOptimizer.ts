e56ac52ef8f456936be1eb81629b8b08bed036c9
julianxcarter <juliancarter55@gmail.com>
2021-03-24 18:14:57 -0400

    Further bug fixes

--- src/optimizer/plugins/SimplifyArrayIndexingOptimizer.ts
+++ src/optimizer/plugins/SimplifyArrayIndexingOptimizer.ts
@@ -2,6 +2,7 @@ import { fshrules, fhirtypes, utils } from 'fsh-sushi';
 import { OptimizerPlugin } from '../OptimizerPlugin';
 import { Package } from '../../processor';
 import { ExportableCaretValueRule } from '../../exportable';
+import _ from 'lodash';
 
 export default {
   name: 'simplify_array_indexing',
@@ -22,10 +23,10 @@ export default {
       const pathMap: Map<string, number> = new Map();
       const caretPathMap: Map<string, Map<string, number>> = new Map();
       const ruleArr: fshrules.Rule[] = def.rules;
-      const singletonArrayElements: Map<string, fhirtypes.PathPart[]> = new Map();
       const parsedPaths = ruleArr.map((rule: fshrules.Rule) => {
         const parsedPath: { path: fhirtypes.PathPart[]; caretPath?: fhirtypes.PathPart[] } = {
-          path: utils.parseFSHPath(rule.path)
+          path: utils.parseFSHPath(rule.path),
+          caretPath: []
         };
         // If we have a CaretValueRule, we'll need a second round of parsing for the caret path
         if (rule instanceof ExportableCaretValueRule) {
@@ -43,10 +44,10 @@ export default {
       parsedPaths.forEach((parsedRule: any, ruleIndex: number) => {
         const originalRule = def.rules[ruleIndex];
         parsedRule.path.forEach((element: fhirtypes.PathPart) => {
-          applySoftIndexing(element, pathMap, singletonArrayElements);
+          applySoftIndexing(element, pathMap);
         });
 
-        parsedRule.caretPath?.forEach((element: fhirtypes.PathPart) => {
+        parsedRule.caretPath.forEach((element: fhirtypes.PathPart) => {
           // Caret path indexes should only be resolved in the context of a specific path
           // Each normal path has a separate map to keep track of the caret path indexes
           if (!caretPathMap.has(originalRule.path)) {
@@ -54,11 +55,11 @@ export default {
           }
 
           const elementCaretPathMap = caretPathMap.get(originalRule.path);
-          applySoftIndexing(element, elementCaretPathMap, singletonArrayElements);
+          applySoftIndexing(element, elementCaretPathMap);
         });
       });
 
-      removeZeroIndices(singletonArrayElements);
+      removeZeroIndices(parsedPaths);
 
       parsedPaths.forEach((parsedRule: any, ruleIndex: number) => {
         const originalRule = def.rules[ruleIndex];
@@ -67,7 +68,6 @@ export default {
           originalRule.caretPath = utils.assembleFSHPath(parsedRule.caretPath);
         }
       });
-      singletonArrayElements.clear();
     });
   }
 } as OptimizerPlugin;
@@ -89,11 +89,7 @@ function addPrefixes(parsedPath: fhirtypes.PathPart[]) {
  * @param {Map<string, number} pathMap - A map containing an element's name as the key and that element's updated index as the value
  * @param {Map<string, PathPart[]} singletonArrayElements - A map containing a string unique to each element of type array as the key, and an array of PathParts as the value
  */
-function applySoftIndexing(
-  element: fhirtypes.PathPart,
-  pathMap: Map<string, number>,
-  singletonArrayElements: Map<string, fhirtypes.PathPart[]>
-) {
+function applySoftIndexing(element: fhirtypes.PathPart, pathMap: Map<string, number>) {
   // Must account for a pathPart's base name, prior portions of the path, as well as any slices it's contained in.
   const mapName = `${element.prefix ?? ''}.${element.base}|${(element.slices ?? []).join('|')}`;
   const indexRegex = /^[0-9]+$/;
@@ -105,46 +101,70 @@ function applySoftIndexing(
 
   if (!pathMap.has(mapName)) {
     pathMap.set(mapName, parseInt(currentNumericBracket));
-    // If the element contains a zero Index, we'll flag the rule to have the index removed
-    if (currentNumericBracket === '0') {
-      singletonArrayElements.set(mapName, [element]);
-    }
   } else {
     const previousNumericIndex = pathMap.get(mapName);
     if (parseInt(currentNumericBracket) === previousNumericIndex + 1) {
-      if (previousNumericIndex === 0) {
-        singletonArrayElements.delete(mapName);
-      }
       element.brackets[bracketIndex] = '+';
       pathMap.set(mapName, previousNumericIndex + 1);
     } else if (parseInt(currentNumericBracket) === previousNumericIndex) {
       element.brackets[bracketIndex] = '=';
-      if (currentNumericBracket === '0') {
-        // If the element contains a zero Index, we'll flag the rule to have the index removed
-        singletonArrayElements.get(mapName).push(element);
-      }
     } else {
-      singletonArrayElements.delete(mapName);
+      pathMap.set(mapName, parseInt(currentNumericBracket));
     }
   }
 }
 
-/**
- * Removes '[0]' and '[=]' indexing from elements with a single value in their array
- * @param {Map<string, PathPart[]} singletonArrayElements - A map containing a string unique to each element of type array as the key, and an array of PathParts as the value
- */
-function removeZeroIndices(singletonArrayElements: Map<string, fhirtypes.PathPart[]>) {
-  const redundantElementGroups = Array.from(singletonArrayElements.values());
-  redundantElementGroups.forEach(cluster => {
-    cluster.forEach(pathPart => {
-      if (pathPart.brackets.includes('0')) {
-        const zeroIndex = pathPart.brackets.indexOf('0');
-        delete pathPart.brackets[zeroIndex];
+// Removes '[0]' and '[=]' indexing from elements with a single value in their array
+function removeZeroIndices(parsedPaths: any[]) {
+  const regularPathElements = _.flatten(parsedPaths.map(rule => rule.path));
+  const caretPathElements = _.flatten(parsedPaths.map(rule => rule.caretPath));
+
+  const referencePathElements = _.cloneDeep(regularPathElements);
+  const referenceCaretPathElements = _.cloneDeep(caretPathElements);
+
+  regularPathElements.forEach((element: fhirtypes.PathPart) => {
+    if (!element.brackets) {
+      return;
+    }
+    const filteredElements = referencePathElements.filter(
+      e =>
+        e.base === element.base &&
+        e.prefix === element.prefix &&
+        e.brackets &&
+        !(e.brackets.includes('0') || e.brackets.includes('='))
+    );
+    if (filteredElements.length === 0) {
+      if (element.brackets.includes('0')) {
+        const zeroIndex = element.brackets.indexOf('0');
+        delete element.brackets[zeroIndex];
       }
-      if (pathPart.brackets.includes('=')) {
-        const equalsIndex = pathPart.brackets.indexOf('=');
-        delete pathPart.brackets[equalsIndex];
+      if (element.brackets.includes('=')) {
+        const equalsIndex = element.brackets.indexOf('=');
+        delete element.brackets[equalsIndex];
       }
-    });
+    }
+  });
+
+  caretPathElements.forEach((element: fhirtypes.PathPart) => {
+    if (!element.brackets) {
+      return;
+    }
+    const filteredElements = referenceCaretPathElements.filter(
+      e =>
+        e.base === element.base &&
+        e.prefix === element.prefix &&
+        e.brackets &&
+        !(e.brackets.includes('0') || e.brackets.includes('='))
+    );
+    if (filteredElements.length === 0) {
+      if (element.brackets.includes('0')) {
+        const zeroIndex = element.brackets.indexOf('0');
+        delete element.brackets[zeroIndex];
+      }
+      if (element.brackets.includes('=')) {
+        const equalsIndex = element.brackets.indexOf('=');
+        delete element.brackets[equalsIndex];
+      }
+    }
   });
 }
