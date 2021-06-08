f9f604d12e7547ad289ccaaeae1db6a02eee614e
Mint Thompson <mathompson@mitre.org>
2021-06-08 09:22:03 -0400

    Remove toFSH implementation from ExportableMappingRule

--- src/exportable/ExportableMappingRule.ts
+++ src/exportable/ExportableMappingRule.ts
@@ -1,16 +1,8 @@
 import { fshrules } from 'fsh-sushi';
-import { fshifyString } from '../exportable/common';
 import { ExportableRule } from '.';
 
 export class ExportableMappingRule extends fshrules.MappingRule implements ExportableRule {
   constructor(path: string) {
     super(path);
-  }
-
-  toFSH(): string {
-    const path = this.path ? ` ${this.path}` : '';
-    const comment = this.comment ? ` "${fshifyString(this.comment)}"` : '';
-    const language = this.language ? ` ${this.language.toString()}` : '';
-    return `*${path} -> "${fshifyString(this.map)}"${comment}${language}`;
   }
 }
