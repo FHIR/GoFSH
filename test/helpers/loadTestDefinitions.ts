cb547858f4c4447cf57e86935f1714ef8dbc8002
Chris Moesel <cmoesel@mitre.org>
2021-12-10 17:35:33 -0500

    Fix tests broken by changes to SUSHI's loadFromPath

--- test/helpers/loadTestDefinitions.ts
+++ test/helpers/loadTestDefinitions.ts
@@ -3,6 +3,6 @@ import { fhirdefs } from 'fsh-sushi';
 
 export function loadTestDefinitions(): fhirdefs.FHIRDefinitions {
   const defs = new fhirdefs.FHIRDefinitions();
-  fhirdefs.loadFromPath(path.join(__dirname, 'testdefs'), 'testPackage', defs);
+  fhirdefs.loadFromPath(path.join(__dirname), 'testdefs', defs);
   return defs;
 }
