ad1bb8f8905905fd7a24a9f54c1e158a5633f620
Julia Afeltra <30803904+jafeltra@users.noreply.github.com>
2022-03-03 13:04:26 -0500

    Update FSH version to 2.0.0 (#174)

--- src/app.ts
+++ src/app.ts
@@ -23,7 +23,7 @@ import {
 import { Package, AliasProcessor } from './processor';
 import { ExportableAlias } from './exportable';
 
-const FSH_VERSION = '1.2.0';
+const FSH_VERSION = '2.0.0';
 
 app().catch(e => {
   logger.error(`Unexpected error: ${e.message}`);
