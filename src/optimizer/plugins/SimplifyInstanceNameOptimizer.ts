9a6e9fc1a9828123f92f9c4708c8385764d9198e
Chris Moesel <cmoesel@mitre.org>
2023-03-31 11:55:16 -0400

    Update to SUSHI 2.10.0 and Fix Instance Name/Id Problems
    Update from SUSHI 2.8.0 to 2.9.0. This caused problems related to a fix in SUSHI 2.9.0 that made it so an instance's id getter would consult assignment rules for id overrides. This required:
    * setting inline instances via name rather than id
    * updating the name optimizer to fixup changed names in inline instane assignments
    
    There was also some ambiguity / confusion related to a test profile having the name Patient (which conflicts with a resource name), so I updated the fixture and tests to reflect a unique name, and added a new fixture for a profile named Patient to test that name clash condition specifically.

--- src/optimizer/plugins/SimplifyInstanceNameOptimizer.ts
+++ src/optimizer/plugins/SimplifyInstanceNameOptimizer.ts
@@ -1,6 +1,6 @@
 import { OptimizerPlugin } from '../OptimizerPlugin';
 import { Package } from '../../processor';
-import { ExportableAssignmentRule } from '../../exportable';
+import { ExportableAssignmentRule, ExportableCaretValueRule } from '../../exportable';
 import ResolveInstanceOfURLsOptimizer from './ResolveInstanceOfURLsOptimizer';
 
 export default {
@@ -10,25 +10,40 @@ export default {
   runAfter: [ResolveInstanceOfURLsOptimizer.name],
 
   optimize(pkg: Package): void {
-    pkg.instances
-      .filter(i => i.usage !== 'Inline')
-      .forEach((instance, index) => {
-        if (!pkg.instances.find((m, i) => m.id === instance.id && i !== index)) {
-          // If the instance does not have the same id as any other instance, it is safe to
-          // use the id as the name and remove the InstanceOf information from the name
-          instance.name = instance.id;
-        } else if (!instance.name.endsWith(instance.instanceOf)) {
-          // If the instance name isn't simplified, the InstanceOf information is present
-          // If it does not end with the current InstanceOf, an alias was used, and we should use that
-          instance.name = `${instance.id}-of-${instance.instanceOf}`;
-        }
+    const oldNameToNewNameMap = new Map<string, string>();
+    pkg.instances.forEach((instance, index) => {
+      if (!pkg.instances.find((m, i) => m.id === instance.id && i !== index)) {
+        // If the instance does not have the same id as any other instance, it is safe to
+        // use the id as the name and remove the InstanceOf information from the name
+        oldNameToNewNameMap.set(instance.name, instance.id);
+        instance.name = instance.id;
+      } else if (!instance.name.endsWith(instance.instanceOf)) {
+        // If the instance name isn't simplified, the InstanceOf information is present
+        // If it does not end with the current InstanceOf, an alias was used, and we should use that
+        oldNameToNewNameMap.set(instance.name, `${instance.id}-of-${instance.instanceOf}`);
+        instance.name = `${instance.id}-of-${instance.instanceOf}`;
+      }
 
-        if (instance.id != null && instance.id !== instance.name) {
-          // add a rule to set the id so it is not lost
-          const idRule = new ExportableAssignmentRule('id');
-          idRule.value = instance.id;
-          instance.rules.unshift(idRule);
+      if (instance.id != null && instance.id !== instance.name) {
+        // add a rule to set the id so it is not lost
+        const idRule = new ExportableAssignmentRule('id');
+        idRule.value = instance.id;
+        instance.rules.unshift(idRule);
+      }
+    });
+
+    // Fix up inline assignments in types that may have them (see: ConstructInlineInstanceOptimizer)
+    [...pkg.instances, ...pkg.profiles, ...pkg.extensions].forEach(resource => {
+      const inlineAssignmentRules = resource.rules.filter(
+        rule =>
+          (rule instanceof ExportableAssignmentRule || rule instanceof ExportableCaretValueRule) &&
+          rule.isInstance
+      );
+      inlineAssignmentRules.forEach((rule: ExportableAssignmentRule | ExportableCaretValueRule) => {
+        if (oldNameToNewNameMap.has(rule.value as string)) {
+          rule.value = oldNameToNewNameMap.get(rule.value as string);
         }
       });
+    });
   }
 } as OptimizerPlugin;
--- test/optimizer/plugins/SimplifyInstanceNameOptimizer.test.ts
+++ test/optimizer/plugins/SimplifyInstanceNameOptimizer.test.ts
@@ -1,7 +1,12 @@
 import path from 'path';
 import '../../helpers/loggerSpy'; // side-effect: suppresses logs
 import { Package } from '../../../src/processor/Package';
-import { ExportableAssignmentRule, ExportableInstance } from '../../../src/exportable';
+import {
+  ExportableAssignmentRule,
+  ExportableCaretValueRule,
+  ExportableInstance,
+  ExportableProfile
+} from '../../../src/exportable';
 import { MasterFisher } from '../../../src/utils';
 import { loadTestDefinitions, stockLake } from '../../helpers';
 import optimizer from '../../../src/optimizer/plugins/SimplifyInstanceNameOptimizer';
@@ -26,12 +31,23 @@ describe('optimizer', () => {
       const instance3 = new ExportableInstance('MyObservationExample');
       instance3.instanceOf = 'Observation';
       instance3.name = 'MyObservationExample-of-Observation';
+      const bundle = new ExportableInstance('MyBundleExample');
+      bundle.instanceOf = 'Bundle';
+      const bundleRule1 = new ExportableAssignmentRule('entry[0].resource');
+      bundleRule1.isInstance = true;
+      bundleRule1.value = 'MyExample-of-Condition';
+      const bundleRule2 = new ExportableAssignmentRule('entry[1].resource');
+      bundleRule2.isInstance = true;
+      bundleRule2.value = 'MyObservationExample-of-Observation';
+      bundle.rules = [bundleRule1, bundleRule2];
       const myPackage = new Package();
       myPackage.add(instance1);
       myPackage.add(instance2);
       myPackage.add(instance3);
+      myPackage.add(bundle);
       optimizer.optimize(myPackage);
 
+      // Check that instances were renamed when appropriate
       const myExampleIdRule = new ExportableAssignmentRule('id');
       myExampleIdRule.value = 'MyExample';
       const expectedInstance1 = new ExportableInstance('MyExample');
@@ -46,10 +62,21 @@ describe('optimizer', () => {
       expectedInstance3.instanceOf = 'Observation';
       expectedInstance3.name = 'MyObservationExample'; // Name simplification since it didn't conflict with any other instances
       // NOTE: expectedInstance3 should not have id rule since name matches id
+      // Now check that names have been updated in inline assignments
+      const expectedBundle = new ExportableInstance('MyBundleExample');
+      expectedBundle.instanceOf = 'Bundle';
+      const expectedBundleRule1 = new ExportableAssignmentRule('entry[0].resource');
+      expectedBundleRule1.isInstance = true;
+      expectedBundleRule1.value = 'MyExample-of-Condition'; // This name did not get changed
+      const expectedBundleRule2 = new ExportableAssignmentRule('entry[1].resource');
+      expectedBundleRule2.isInstance = true;
+      expectedBundleRule2.value = 'MyObservationExample'; // This name was changed
+      expectedBundle.rules = [expectedBundleRule1, expectedBundleRule2];
       expect(myPackage.instances).toEqual([
         expectedInstance1,
         expectedInstance2,
-        expectedInstance3
+        expectedInstance3,
+        expectedBundle
       ]);
     });
 
@@ -64,14 +91,25 @@ describe('optimizer', () => {
       const instance2 = new ExportableInstance('MyExample');
       instance2.instanceOf = 'http://hl7.org/fhir/us/foo/StructureDefinition/bar-profile';
       instance2.name = 'MyExample-of-http://hl7.org/fhir/us/foo/StructureDefinition/bar-profile';
+      const bundle = new ExportableInstance('MyBundleExample');
+      bundle.instanceOf = 'Bundle';
+      const bundleRule1 = new ExportableAssignmentRule('entry[0].resource');
+      bundleRule1.isInstance = true;
+      bundleRule1.value = 'MyExample-of-http://hl7.org/fhir/us/foo/StructureDefinition/foo-profile';
+      const bundleRule2 = new ExportableAssignmentRule('entry[1].resource');
+      bundleRule2.isInstance = true;
+      bundleRule2.value = 'MyExample-of-http://hl7.org/fhir/us/foo/StructureDefinition/bar-profile';
+      bundle.rules = [bundleRule1, bundleRule2];
       const myPackage = new Package();
       myPackage.add(instance1);
       myPackage.add(instance2);
+      myPackage.add(bundle);
 
       // Resolve InstanceOf URLs first, then simplify Instance names to check they are updated according to the alias
       ResolveInstanceOfURLsOptimizer.optimize(myPackage, fisher);
       optimizer.optimize(myPackage);
 
+      // Check that instances were renamed when appropriate
       const myExampleIdRule = new ExportableAssignmentRule('id');
       myExampleIdRule.value = 'MyExample';
       const expectedInstance1 = new ExportableInstance('MyExample');
@@ -82,7 +120,88 @@ describe('optimizer', () => {
       expectedInstance2.instanceOf = '$bar-profile';
       expectedInstance2.name = 'MyExample-of-$bar-profile';
       expectedInstance2.rules = [myExampleIdRule];
-      expect(myPackage.instances).toEqual([expectedInstance1, expectedInstance2]);
+      // Now check that names have been updated in inline assignments
+      const expectedBundle = new ExportableInstance('MyBundleExample');
+      expectedBundle.instanceOf = 'Bundle';
+      const expectedBundleRule1 = new ExportableAssignmentRule('entry[0].resource');
+      expectedBundleRule1.isInstance = true;
+      expectedBundleRule1.value = 'MyExample-of-$foo-profile';
+      const expectedBundleRule2 = new ExportableAssignmentRule('entry[1].resource');
+      expectedBundleRule2.isInstance = true;
+      expectedBundleRule2.value = 'MyExample-of-$bar-profile';
+      expectedBundle.rules = [expectedBundleRule1, expectedBundleRule2];
+      expect(myPackage.instances).toEqual([expectedInstance1, expectedInstance2, expectedBundle]);
+    });
+
+    it('should updated inlined contained rules in a profile when the inline instance name changes', () => {
+      const instance1 = new ExportableInstance('AmazingThings');
+      instance1.instanceOf = 'CodeSystem';
+      instance1.name = 'AmazingThings-of-CodeSystem';
+      const instance2 = new ExportableInstance('AmazingThings');
+      instance2.instanceOf = 'ValueSet';
+      instance2.name = 'AmazingThings-of-ValueSet';
+      const instance3 = new ExportableInstance('OtherAmazingThings');
+      instance3.instanceOf = 'ValueSet';
+      instance3.name = 'OtherAmazingThings-of-ValueSet';
+      const profile = new ExportableProfile('MyObservationProfile');
+      profile.parent = 'Observation';
+      const profileContainedRule1 = new ExportableCaretValueRule('.');
+      profileContainedRule1.caretPath = 'contained[0]';
+      profileContainedRule1.isInstance = true;
+      profileContainedRule1.value = 'AmazingThings-of-CodeSystem';
+      const profileContainedRule2 = new ExportableCaretValueRule('.');
+      profileContainedRule2.caretPath = 'contained[1]';
+      profileContainedRule2.isInstance = true;
+      profileContainedRule2.value = 'AmazingThings-of-ValueSet';
+      const profileContainedRule3 = new ExportableCaretValueRule('.');
+      profileContainedRule3.caretPath = 'contained[2]';
+      profileContainedRule3.isInstance = true;
+      profileContainedRule3.value = 'OtherAmazingThings-of-ValueSet';
+      profile.rules = [profileContainedRule1, profileContainedRule2, profileContainedRule3];
+      const myPackage = new Package();
+      myPackage.add(instance1);
+      myPackage.add(instance2);
+      myPackage.add(instance3);
+      myPackage.add(profile);
+      optimizer.optimize(myPackage);
+
+      // Check that instances were renamed when appropriate
+      const amazingThingsIdRule = new ExportableAssignmentRule('id');
+      amazingThingsIdRule.value = 'AmazingThings';
+      const expectedInstance1 = new ExportableInstance('AmazingThings');
+      expectedInstance1.instanceOf = 'CodeSystem';
+      expectedInstance1.name = 'AmazingThings-of-CodeSystem'; // No name simplification since it will conflict with other instances
+      expectedInstance1.rules = [amazingThingsIdRule];
+      const expectedInstance2 = new ExportableInstance('AmazingThings');
+      expectedInstance2.instanceOf = 'ValueSet';
+      expectedInstance2.name = 'AmazingThings-of-ValueSet'; // No name simplification since it will conflict with other instances
+      expectedInstance2.rules = [amazingThingsIdRule];
+      const expectedInstance3 = new ExportableInstance('OtherAmazingThings');
+      expectedInstance3.instanceOf = 'ValueSet';
+      expectedInstance3.name = 'OtherAmazingThings'; // Name simplification since it didn't conflict with any other instances
+      // NOTE: expectedInstance3 should not have id rule since name matches id
+      // Now check that names have been updated in inline assignments
+      const expectedProfile = new ExportableProfile('MyObservationProfile');
+      expectedProfile.parent = 'Observation';
+      const expectedProfileRule1 = new ExportableCaretValueRule('.');
+      expectedProfileRule1.caretPath = 'contained[0]';
+      expectedProfileRule1.isInstance = true;
+      expectedProfileRule1.value = 'AmazingThings-of-CodeSystem'; // This name did not get changed
+      const expectedProfileRule2 = new ExportableCaretValueRule('.');
+      expectedProfileRule2.caretPath = 'contained[1]';
+      expectedProfileRule2.isInstance = true;
+      expectedProfileRule2.value = 'AmazingThings-of-ValueSet'; // This name did not get changed
+      const expectedProfileRule3 = new ExportableCaretValueRule('.');
+      expectedProfileRule3.caretPath = 'contained[2]';
+      expectedProfileRule3.isInstance = true;
+      expectedProfileRule3.value = 'OtherAmazingThings'; // This name was changed
+      expectedProfile.rules = [expectedProfileRule1, expectedProfileRule2, expectedProfileRule3];
+      expect(myPackage.instances).toEqual([
+        expectedInstance1,
+        expectedInstance2,
+        expectedInstance3
+      ]);
+      expect(myPackage.profiles).toEqual([expectedProfile]);
     });
   });
 });
