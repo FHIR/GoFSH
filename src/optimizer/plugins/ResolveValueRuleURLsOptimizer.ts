36acbedb913531bfea08250a589a8ee958b486ad
Mint Thompson <mathompson@mitre.org>
2023-04-20 09:25:20 -0400

    Use name of local CodeSystem when optimizing assigned code (#215)
    * Use name of local CodeSystem when optimizing assigned code
    
    A url for the system of an assigned code used in an AssignmentRule or
    CaretValueRule will be turned into the name of a locally defined
    CodeSystem when possible. A local name can be used regardless of the
    option to create aliases.
    
    * Only use name for local CodeSystems
    
    * Clarify name and description
    
    * keep system intact when it is not a web URL

--- src/optimizer/plugins/ResolveValueRuleURLsOptimizer.ts
+++ src/optimizer/plugins/ResolveValueRuleURLsOptimizer.ts
@@ -1,4 +1,4 @@
-import { fshtypes } from 'fsh-sushi';
+import { fshtypes, utils } from 'fsh-sushi';
 import { OptimizerPlugin } from '../OptimizerPlugin';
 import { Package } from '../../processor';
 import {
@@ -8,14 +8,15 @@ import {
 } from '../../exportable';
 import { resolveAliasFromURL } from '../utils';
 import CombineCodingAndQuantityValuesOptimizer from './CombineCodingAndQuantityValuesOptimizer';
-import { ProcessingOptions } from '../../utils';
+import { ProcessingOptions, MasterFisher } from '../../utils';
 
 export default {
-  name: 'resolve_value_rule_urls',
-  description: 'Replace URLs in caret and assignment rules with their names or aliases',
+  name: 'resolve_value_rule_system_urls_for_codes',
+  description:
+    'Replace system URLs in code values in caret and assignment rules with their names or aliases',
   runAfter: [CombineCodingAndQuantityValuesOptimizer.name],
 
-  optimize(pkg: Package): void {
+  optimize(pkg: Package, fisher: MasterFisher, options: ProcessingOptions = {}): void {
     [
       ...pkg.instances,
       ...pkg.profiles,
@@ -28,13 +29,18 @@ export default {
           (rule instanceof ExportableAssignmentRule || rule instanceof ExportableCaretValueRule) &&
           rule.value instanceof fshtypes.FshCode
         ) {
-          rule.value.system =
-            resolveAliasFromURL(rule.value.system, pkg.aliases) ?? rule.value.system;
+          const localSystem = fisher.lakeOfFHIR.fishForFHIR(
+            rule.value.system,
+            utils.Type.CodeSystem
+          );
+          if (localSystem?.name) {
+            rule.value.system = localSystem.name;
+          } else if (options.alias ?? true) {
+            rule.value.system =
+              resolveAliasFromURL(rule.value.system, pkg.aliases) ?? rule.value.system;
+          }
         }
       });
     });
-  },
-  isEnabled(options: ProcessingOptions): boolean {
-    return options.alias === true;
   }
 } as OptimizerPlugin;
