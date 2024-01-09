8dd596a00b5497cadf573684c670f4e7206f8b88
Mint Thompson <mathompson@mitre.org>
2024-01-08 19:13:35 -0500

    Remove metadataToFSH override from ExportableInvariant

--- src/exportable/ExportableInvariant.ts
+++ src/exportable/ExportableInvariant.ts
@@ -1,36 +1,10 @@
 import { fshtypes } from 'fsh-sushi';
 import { Exportable, ExportableAssignmentRule, ExportableInsertRule } from '.';
-import { fshifyString } from './common';
-import { EOL } from 'os';
 
 export class ExportableInvariant extends fshtypes.Invariant implements Exportable {
   rules: (ExportableAssignmentRule | ExportableInsertRule)[];
 
   constructor(name: string) {
     super(name);
-  }
-
-  metadataToFSH(): string {
-    const resultLines: string[] = [];
-    resultLines.push(`Invariant: ${this.name}`);
-    if (this.description) {
-      // Description can be a multiline string.
-      // If it contains newline characters, treat it as a multiline string.
-      if (this.description.indexOf('\n') > -1) {
-        resultLines.push(`Description: """${this.description}"""`);
-      } else {
-        resultLines.push(`Description: "${fshifyString(this.description)}"`);
-      }
-    }
-    if (this.severity) {
-      resultLines.push(`* severity = ${this.severity}`);
-    }
-    if (this.expression) {
-      resultLines.push(`* expression = "${fshifyString(this.expression)}"`);
-    }
-    if (this.xpath) {
-      resultLines.push(`* xpath = "${fshifyString(this.xpath)}"`);
-    }
-    return resultLines.join(EOL);
   }
 }
