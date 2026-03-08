5ab4eaa52ac4d6e539c8c3efdace0816dcbd31d3
Chris Moesel <cmoesel@users.noreply.github.com>
2026-03-08 15:56:22 -0400

    update dev dependencies

---
+++ src/types/profoundlogic_hogan.d.ts
@@ -0,0 +1,6 @@
+// Hogan is a dependency of diff2html. This type declaration is needed to keep tsc happy.
+declare module '@profoundlogic/hogan' {
+  export class Template {}
+  export class Partials {}
+  export class Context {}
+}
